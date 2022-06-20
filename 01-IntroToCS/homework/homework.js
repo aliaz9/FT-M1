501aa4ddee7bd4c60739486602571ce5c865a8b2
aliaz9 <alina.alvarez9@gmail.com>
2022-06-20 17:03:44 -0300

    Update homework.js

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,48 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  arrayBinario = [128, 64, 32, 16, 8, 4, 2, 0]
+  decimal = 0;
+  // Ejemplo> 0 1 0 0 1 0 0 1
+
+  for (let i = 0; i < 7; i++) {
+
+      decimal = decimal + num[i] * arrayBinario[j];
+      arrayBinario[j]++
+
+  }
+
 }
+
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+
+  decimal = 0;
+  // Ejemplo> 0 1 0 0 1 0 0 1
+
+  for (let i = 0; i < 7; i++) {
+
+      decimal = decimal + num[i] * 2 * num.length - i ;
+
+  }
+
+}
+
+
 
 function DecimalABinario(num) {
   // tu codigo aca
+
+  arrayBinario = [];
+  nuevoNum = 0;
+
+  while (num >= 2) {
+
+    arrayBinario.push(num % 2);
+    nuevoNum = Math.floor(num / 2);
+
+  }
 
 }
 
@@ -14,4 +52,7 @@ function DecimalABinario(num) {
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
+
+
+  
 }
